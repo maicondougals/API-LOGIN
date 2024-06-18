36816f3849d2182c3dabb536802eeac5afb464b1
Maicon Douglas <maiconboy11@gmail.com>
2024-06-18 01:51:28 -0300

    1

--- app.js
+++ app.js
@@ -1,158 +1,141 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const path = require('path');
 
-const app = express()
-
+const app = express();
+const secret = process.env.SECRET; //Código secreto encontrado no .env para maior segurança
 const cors = require('cors');
-
-
-
-
 app.use(cors());
 
+app.use(express.json());
 
-app.use(express.json())
+const User = require('./models/User');
 
-const User = require('./models/User')
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: 'bem vindo' }); //rota teste
+});
 
-app.get('/', (req, res) =>{
-    res.status(200).json({msg: 'bem vindo'})
-})
+app.get('/user/:id', checkToken, async (req, res) => { //rota para acessar api individual de cada usuário com verificação de token
+    const id = req.params.id;
 
+    const user = await User.findById(id, '-password');
 
-app.get('/user/:id', checkToken, async(req,res)=>{
-    const id = req.params.id
-
-    const user = await User.findById(id, '-password')
-
-    if(!user){
-        return res.status(404).json({msg: 'usuário não encontrado'})
+    if (!user) {
+        return res.status(404).json({ msg: 'usuário não encontrado' });
     }
 
-    res.status(200).json({user})
-})
-app.get('/restricted', checkToken, (req, res) => {
-    res.sendFile(path.join(__dirname, 'restricted.html'));
-  });
+    res.status(200).json({ user });
+});
 
+function checkToken(req, res, next) {
+    
+    const authHeader = req.headers.authorization; //captando o cabeçalho 
+  
 
-function checkToken(req, res, next){
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-
-    if(!token){
-        return res.status(401).json({msg:'Acesso negado!'})
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Token não fornecido.' }); //verificando se há algo no cabeçalho
     }
 
-    try{
-        const secret = process.env.SECRET
+    const token = authHeader.split(' ')[1]; //captar o token "bearer : 123" 123 seria o token
+  
 
-        jwt.verify(token, secret)
-
-        next()
-
-
-    }catch(error){
-        res.status(400).json({msg: 'Token inválido!'})
+    if (!token) {
+        return res.status(401).json({ message: 'Token incorreto.' }); //verifica se o token está correto
     }
-}
+  
+    jwt.verify(token, secret, (err, decoded) => {
+        if (err) {
+            console.error('Erro na verificação do token:', err); //erro de verificação do token
+            return res.status(403).json({ message: 'Falha na autenticação do token.' });
+        }
+        req.user = decoded;
+        next();
+    });
+  }
 
 
+app.post('/auth/register', async (req, res) => {
+    const { name, email, password, confirmpassword } = req.body; //captando strings que serão necessárias 
 
-app.post('/auth/register', async(req, res) =>{
-    const{name, email, password, confirmpassword} = req.body
-
-    if(!name){
-        return res.status(422).json({msg : 'O nome é obrigatório'})
+    if (!name) {
+        return res.status(422).json({ msg: 'O nome é obrigatório' });
     }
-    if(!email){
-        return res.status(422).json({msg : 'O email é obrigatório'})
+    if (!email) {
+        return res.status(422).json({ msg: 'O email é obrigatório' });
     }
-    if(!password){
-        return res.status(422).json({msg : 'A senha é obrigatória'})
+    if (!password) {
+        return res.status(422).json({ msg: 'A senha é obrigatória' });
     }
-    if(password !== confirmpassword){
-        return res.status(422).json({msg : 'As senhas não conferem'})
-    }
-
-
-    const userExist = await User.findOne({email: email})
-
-    if(userExist){
-        return res.status(422).json({msg : 'Utilize outro email'})
+    if (password !== confirmpassword) {
+        return res.status(422).json({ msg: 'As senhas não conferem' });
     }
 
+    const userExist = await User.findOne({ email: email }); //verifica se o o email exite no banco de dados
 
-    const salt = await bcrypt.genSalt(12)
-    const passwordHash = await bcrypt.hash(password, salt)
+    if (userExist) {
+        return res.status(422).json({ msg: 'Utilize outro email' });
+    }
 
-
-
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
 
     const user = new User({
-        name, 
+        name,
         email,
         password: passwordHash,
-    })
+    });
 
-    try{
-        await user.save()
-        res.status(201).json({msg: 'Usuário cadastrado com sucesso!'})
-    }catch(error){
-        console.log(error)
-        res.status(500).json({msg: 'Aconteceu algum erro no servidor!'})
+    try {
+        await user.save();
+        res.status(201).json({ msg: 'Usuário cadastrado com sucesso!' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'Aconteceu algum erro no servidor!' });
     }
+});
 
+app.post('/auth/login', async (req, res) => {
+    const { email, password } = req.body;
 
-})
+    try {
+        const user = await User.findOne({email: email})
 
-app.post('/auth/login', async(req, res) =>{
-    const { email, password} = req.body
+        if (!user) {
+            return res.status(404).json({ msg: 'Usuário não encontrado' });
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+            if (!isPasswordCorrect) {
+            return res.status(422).json({ msg: 'Senha incorreta' });
+        }
 
-    const user = await User.findOne({email: email})
+     
+       
+        const token = jwt.sign({ id: user.id, email: user.email }, secret, { expiresIn: '1h' });
 
-    if(!user ){
-        return res.status(404).json({msg : 'Usuário não encontrado'})
+        res.status(200).json({ msg: 'Autenticação realizada com sucesso', token });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ msg: 'Aconteceu algum erro no servidor!' });
     }
+});
 
-    const checkPassord = await bcrypt.compare(password, user.password)
-    if(!checkPassord){
-        return res.status(422).json({msg : 'Senha incorreta'})
-    }
+app.get('/restricted', checkToken,  (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/restricted.html'));
+});
 
-    try{
-        const secret = process.env.SECRET
-
-        const token = jwt.sign(
-            {
-                id: user._id,
-            },
-            secret,
-        
-        )
-        res.status(200).json({msg: 'Autenticação realizada com sucesso', token})
-    }catch(error){
-        console.log(error)
-        res.status(500).json({msg: 'Aconteceu algum erro no servidor!'})
-    }
-
-
-})
-
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.BR_PASS
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.BR_PASS;
 
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.nskqgcr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
-.then(()=>{
-    app.listen(3000)
-    console.log('conectou ao banco!')
-})
-.catch((err)=>{
-    console.log('não conectou ao banco!', err)
-})
-
-
-
+    .then(() => {
+        const PORT = 3001;
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log('não conectou ao banco!', err);
+    });
